4c1e355ffe5f9d5d4966b9b435f5328e47a0cb33
user <user@DESKTOP-RHSH073>
2023-05-03 10:51:42 +0200

    Add project files.

---
+++ Projekt.API/Migrations/20230503084542_ProjMig.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Projekt.API.Migrations
+{
+    public partial class ProjMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Universities");
+
+            migrationBuilder.DropColumn(
+                name: "GPA",
+                table: "Universities");
+
+            migrationBuilder.RenameColumn(
+                name: "UniversityNumber",
+                table: "Universities",
+                newName: "UniversityName");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Universities",
+                newName: "Code");
+
+            migrationBuilder.RenameColumn(
+                name: "DOB",
+                table: "Universities",
+                newName: "UniversityDateCreated");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Dean",
+                table: "Faculties",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Dean",
+                table: "Faculties");
+
+            migrationBuilder.RenameColumn(
+                name: "UniversityName",
+                table: "Universities",
+                newName: "UniversityNumber");
+
+            migrationBuilder.RenameColumn(
+                name: "UniversityDateCreated",
+                table: "Universities",
+                newName: "DOB");
+
+            migrationBuilder.RenameColumn(
+                name: "Code",
+                table: "Universities",
+                newName: "LastName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Universities",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<double>(
+                name: "GPA",
+                table: "Universities",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
---
+++ Projekt.API/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using Projekt.API.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Connection String
+string dbConnectionString = "Data Source=DESKTOP-RHSH073\\sqlexpress;Initial Catalog=ProjectDB;Integrated Security=True;Pooling=False";
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(dbConnectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
